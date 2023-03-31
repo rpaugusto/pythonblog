c4ce478a905f414f8546923aa9e11d7462b2886d
rpaugusto <rp.augusto@hotmail.com>
2023-03-31 17:07:23 -0300

    update 1

--- hello.py
+++ hello.py
@@ -1,12 +1,25 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 #Creata a Flask Instance
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "mySuperSecreteKey!"
+
+# Create a Form Class
+class NameForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 #Create a route decorator
 @app.route('/')
 def index():
-    return render_template('base.html')
+    return render_template('index.html')
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('profile.html', user=name)
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -16,5 +29,16 @@ def page_not_found(e):
 def page_not_found(e):
     return render_template('500.html'),500
 
+@app.route('/name', methods=['GET','POST'])
+def name():
+    name = None
+    form = NameForm()
+
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+        
+    return render_template('name.html', name=name, form=form)
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
